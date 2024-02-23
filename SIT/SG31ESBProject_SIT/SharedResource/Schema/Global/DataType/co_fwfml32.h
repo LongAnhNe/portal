bef0eef9d2a5d38a07778463460acfd84be8a6d5
Longanhne <longnh11092000@gmail.com>
2024-02-23 14:38:18 +0700

    portal

---
+++ SIT/SG31ESBProject_SIT/SharedResource/Schema/Global/DataType/co_fwfml32.h
@@ -0,0 +1,6 @@
+/*	fname	fldid            */
+/*	-----	-----            */
+#define	ROUTING_CODE	((FLDID32)167773161)	/* number: 1001	 type: string */
+#define	ERROR_CODE	((FLDID32)167773162)	/* number: 1002	 type: string */
+#define	OPTION	((FLDID32)167773163)	/* number: 1003	 type: string */
+#define	DATA	((FLDID32)167773164)	/* number: 1004	 type: string */
